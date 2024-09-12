d192329802e9511aba8cc0d6a78669352046e167
nikoyangar <nikoyangar@gmail.com>
2024-09-12 17:19:14 +0400

    add new samples

---
+++ Lesson_AsyncAwait/001_AsyncAwait/Program.cs
@@ -0,0 +1,53 @@
+﻿using System.Diagnostics;
+
+namespace _001_AsyncAwait
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //synchronously
+            Stopwatch stopwatch = new Stopwatch();
+            stopwatch.Start();
+            var result = CalculateLength("Hello, World!");
+            Print(result);
+            Console.WriteLine("Process finished");
+            stopwatch.Stop();
+
+            //asynchronously
+            var task = Task.Run(() => CalculateLength("Hello, World!"))
+                .ContinueWith(completedtask => Print(completedtask.Result))
+                .ContinueWith(previuscontiuation => Console.WriteLine("Process finished"));
+
+            //it doesn't need calculate length and print 
+            string userInput;
+            do
+            {
+                Console.WriteLine("Enter command: ");
+                userInput = Console.ReadLine();
+            } while (userInput != "exit");
+
+            Console.WriteLine($"All tasks completed. Elapsed time: {stopwatch.ElapsedMilliseconds} ms");
+            Console.WriteLine("Main finished");
+            Console.ReadKey();
+
+        }
+        static int CalculateLength(string? text)
+        {
+            if (text == null)
+            {
+                return 0;
+            }
+            Console.WriteLine($"Start CalculateLength in ManagedThreadId: {Thread.CurrentThread.ManagedThreadId}");
+            Thread.Sleep(3000);
+            return text.Length;
+        }
+        static void Print(int length)
+        {
+            Console.WriteLine($"Start Print in ManagedThreadId: {Thread.CurrentThread.ManagedThreadId}");
+            Thread.Sleep(3000);
+            Console.WriteLine($"Length: {length}");
+        }
+    }
+
+}
---
+++ Lesson_AsyncAwait/002_AsyncAwait/Program.cs
@@ -0,0 +1,33 @@
+﻿namespace _002_AsyncAwait
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var task = RunHeavyProcess();
+            Console.WriteLine("Doing other Work");
+            Console.WriteLine("Doing even more other Work");
+            Console.ReadKey();
+        }
+        static Task RunHeavyProcess()
+        {
+            return Task.Run(() => HeavyProcess())
+                .ContinueWith(completedTask => Console.WriteLine(completedTask.Result));
+        }
+        //await keyword means.start HeavyProcess but don't just wait here for it to be finished.
+        //while it is running, do other work. etc go to caller method and do other work.
+        //Don't process the line after await keyword before the result of HeavyProcess is ready.
+        //static Task RunHeavyProcess()
+        //{
+        //    var result = await HeavyProcess(); 
+        //    Console.WriteLine(result);
+        //}
+        static string HeavyProcess()
+        {
+            Console.WriteLine("Heavy process started");
+            Thread.Sleep(5000);
+            Console.WriteLine("Heavy process finished");
+            return "Done";
+        }
+    }
+}
