81c3e7f9b9f4580b431555fd2384cfc004b753b8
nikoyangar <nikoyangar@gmail.com>
2024-09-12 15:47:07 +0400

    add examples on Synchronization

---
+++ Lesson_Synchronization/001_Synchronization/Program.cs
@@ -0,0 +1,49 @@
+﻿namespace _001_Synchronization
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // synchron
+            Counter counter = new Counter();
+            for (int i = 0; i < 1000; i++)
+            {
+                counter.Increment();
+            }
+            for (int i = 0; i < 1000; i++)
+            {
+                counter.Decrement();
+            }
+            Console.WriteLine(counter.Count);
+
+            Console.WriteLine("--------------------------");
+
+            ////asynchron
+            //Counter counter1 = new Counter();
+            //var tasks = new List<Task>();
+            //for (int i = 0; i < 1000; i++)
+            //{
+            //    tasks.Add(Task.Run(() => counter1.Increment()));
+            //}
+            //for (int i = 0; i < 1000; i++)
+            //{
+            //    tasks.Add(Task.Run(() => counter1.Decrement()));
+            //}
+            //Task.WaitAll(tasks.ToArray());
+            //Console.WriteLine(counter1.Count);
+            //Console.ReadKey();
+        }
+    }
+    public class Counter
+    {
+        public int Count { get; set; }
+        public void Increment()
+        {
+            Count++;
+        }
+        public void Decrement()
+        {
+            Count--;
+        }
+    }
+}
---
+++ Lesson_Synchronization/002_Synchronization/Program.cs
@@ -0,0 +1,42 @@
+﻿namespace _002_Synchronization
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //asynchron
+            Counter counter1 = new Counter();
+            var tasks = new List<Task>();
+            for (int i = 0; i < 1000; i++)
+            {
+                tasks.Add(Task.Run(() => counter1.Increment()));
+            }
+            for (int i = 0; i < 1000; i++)
+            {
+                tasks.Add(Task.Run(() => counter1.Decrement()));
+            }
+            Task.WaitAll(tasks.ToArray());
+            Console.WriteLine(counter1.Count);
+            Console.ReadKey();
+        }
+    }
+    public class Counter
+    {
+        public object lockObject = new object();
+        public int Count { get; set; }
+        public void Increment()
+        {
+            lock (lockObject)
+            {
+                Count++;
+            }
+        }
+        public void Decrement()
+        {
+            lock (lockObject)
+            {
+                Count--;
+            }
+        }
+    }
+}
